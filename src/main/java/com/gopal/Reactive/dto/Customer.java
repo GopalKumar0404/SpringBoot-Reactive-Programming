a45467a20d1348d8baf16bd3fe4f51b70fa2509b
Gopal Kumar <gopalkumar24680@outlook.com>
2023-10-21 07:50:39 +0530

    "Added Router Function in Reactive Programming of Spring Boot"

---
+++ src/main/java/com/gopal/Reactive/dto/Customer.java
@@ -0,0 +1,14 @@
+package com.gopal.Reactive.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Customer {
+
+    int id;
+    String name;
+}
---
+++ src/main/java/com/gopal/Reactive/router/RouterConfig.java
@@ -0,0 +1,32 @@
+package com.gopal.Reactive.router;
+
+
+import com.gopal.Reactive.handle.CustomerHandler;
+import com.gopal.Reactive.handle.CustomerStreamHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterConfig {
+
+    @Autowired
+    private CustomerHandler handler;
+
+    @Autowired
+    private CustomerStreamHandler streamHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(){
+        return RouterFunctions.route()
+                .GET("/router/customers",handler::loadCustomers)
+                .GET("/router/customers/stream",streamHandler::getCustomers)
+                .GET("/router/customer/{input}",handler::findCustomer)
+                .POST("/router/customer/save",handler::saveCustomer)
+                .build();
+
+    }
+}
